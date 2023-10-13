c84c193ae4cfbc4e78422adebc78b004ef08de12
Denis Efremov <efremov@novuscom.net>
2023-10-13 11:16:03 -0700

    Initial Commit

---
+++ InstagramAsync/InstagramAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAsyncApp.swift
+//  InstagramAsync
+//
+//  Created by Denis Efremov on 2023-10-13.
+//
+
+import SwiftUI
+
+@main
+struct InstagramAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
