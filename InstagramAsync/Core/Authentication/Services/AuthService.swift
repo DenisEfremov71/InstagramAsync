41bc745fe7d2d44c099065a9679a4912e97e3ee1
Denis Efremov <efremov@novuscom.net>
2023-10-16 15:27:54 -0700

    Added uploading user data

--- InstagramAsync/Core/Authentication/Services/AuthService.swift
+++ InstagramAsync/Core/Authentication/Services/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestoreSwift
+import Firebase
 
 class AuthService {
 
@@ -15,13 +17,13 @@ class AuthService {
     static let shared = AuthService()
 
     init() {
-        self.userSession = Auth.auth().currentUser
+        userSession = Auth.auth().currentUser
     }
 
     func login(withEmail email: String, password: String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
-            self.userSession = result.user
+            userSession = result.user
         } catch {
             print("DEBUG: Failed to log in with error \(error.localizedDescription)")
         }
@@ -30,7 +32,10 @@ class AuthService {
     func createUser(email: String, password: String, username: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
-            self.userSession = result.user
+            userSession = result.user
+            print("DEBUG: did create user..")
+            await uploadUserData(uid: result.user.uid, username: username, email: email)
+            print("DEBUG: did upload user data..")
         } catch {
             print("DEBUG: Failed to register user with error \(error.localizedDescription)")
         }
@@ -43,6 +48,16 @@ class AuthService {
     func signout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else {
+            return
+        }
+
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
 
 }
